47385ff3eb2812169592395dac06ede5cf1dff3e
piyushxyz1 <piyushnegi033@gmail.com>
2024-09-11 18:57:11 +0530

    added

---
+++ frontend/dist/precache-manifest.ea05cbc7b7f186360abd707d4e239394.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69c41c887fc68f3dd27f44996261d0bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4005f0a1a8cc581fb27e",
+    "url": "/static/css/main.efada0f5.chunk.css"
+  },
+  {
+    "revision": "eb0cb0fa5a40c2365df9",
+    "url": "/static/js/2.5f7fc32e.chunk.js"
+  },
+  {
+    "revision": "cf5bad70b64104c728026f4ce2304e20",
+    "url": "/static/js/2.5f7fc32e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4005f0a1a8cc581fb27e",
+    "url": "/static/js/main.9cce552f.chunk.js"
+  },
+  {
+    "revision": "0b6b46ef1e17d32e6df4",
+    "url": "/static/js/runtime-main.bce919bb.js"
+  }
+]);
